8ccb53857137189b827dd5d9d9fab2e25c79e50c
Christopher Bayliss <cabayliss@gmail.com>
2026-01-29 12:58:00 +0000

    Added API project

---
+++ MovieAPI/MovieAPI/Controllers/MoviesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieAPI.Controllers
+{
+    [Route("api/movies")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        [HttpGet("popular")]
+        public IActionResult GetPopularMovies(
+        [FromQuery] int page = 1,
+        [FromQuery] int pageSize = 20)
+        {
+            var movies = new[]
+            {
+            new
+            {
+                Id = 1,
+                Title = "Scary Movie",
+                PosterUrl = null as string,
+                ReleaseDate = "2024-01-01",
+                Rating = 7.8
+            },
+            new
+            {
+                Id = 2,
+                Title = "Not Another Teen Movie",
+                PosterUrl = null as string,
+                ReleaseDate = "2023-10-15",
+                Rating = 6.9
+            }
+        };
+
+            return Ok(new
+            {
+                Page = page,
+                PageSize = pageSize,
+                TotalPages = 1,
+                TotalResults = movies.Length,
+                Items = movies
+            });
+        }
+    }
+}
